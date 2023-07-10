d88e0ba81becf7a5eade7ffc43578f2a1aa48682
Denis Dudar <denis.dudar11@gmail.com>
2023-07-10 11:14:47 +0300

    Completed all tasks from 3-extra folder

--- 3-extra/2-sorting-algorithm.js
+++ 3-extra/2-sorting-algorithm.js
@@ -14,7 +14,15 @@ You don't have to worry about making this algorithm work fast! The idea is to ge
 "think" like a computer and practice your knowledge of basic JavaScript.
 */
 
-function sortAges(arr) {}
+function sortAges(arr) {
+  let result = [];
+  for (item of arr) {
+    if (typeof(item) === "number") {
+      result.push(item);
+    }
+  }
+  return result.sort();
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== */
 
